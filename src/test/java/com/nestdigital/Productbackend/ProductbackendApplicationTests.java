340666a98a1be1e43d57af3e051a2ab9bb4fda00
Jithin Ramesh <109571606+jithinramesh@users.noreply.github.com>
2022-10-19 23:43:32 +0530

    fa

---
+++ src/test/java/com/nestdigital/Productbackend/ProductbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.Productbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
